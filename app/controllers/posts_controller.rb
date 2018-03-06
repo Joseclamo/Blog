55de55c5fcf6b11e7513c7e28db725a899a65107
Jose <joseclamo@gmail.com>
2018-03-06 14:09:06 +0100

    paginacion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc).page(params[:page])
   end
 
   # GET /posts/1
